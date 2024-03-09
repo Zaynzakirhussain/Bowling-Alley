317b118f5cc93ad9040f92bbff23e6c18a56afea
Zayn <rizwana.parveen@gmail.com>
2024-03-09 23:58:24 +0530

    first commit

--- bowling.js
+++ bowling.js
@@ -9,13 +9,14 @@ AFRAME.registerComponent("bowl", {
                 var ball = document.createElement("a-entity");
                 ball.setAttribute("geometry", {
                     primitive: "sphere",
-                    radius: 0.25
+                    radius: 0.42
                 });
                 ball.setAttribute("material", "color", "purple");
 
-                ball.setAttribute("dynamic-body", { shape: "sphere", mass: "0" })
+                ball.setAttribute("dynamic-body", { shape: "sphere", mass: "1" })
 
                 var cam = document.querySelector("#camera");
+                var grd = document.querySelector("#ground");
                 var camera = document.querySelector("#camera").object3D;
                 var scene = document.querySelector("#scene");
 
@@ -23,17 +24,42 @@ AFRAME.registerComponent("bowl", {
                 var direction = new THREE.Vector3();
                 camera.getWorldDirection(direction);
                 pos = cam.getAttribute("position");
+                grdy = grd.getAttribute("position");
 
                 ball.setAttribute("position", {
                     x: pos.x,
-                    y: pos.y,
+                    y: grdy.y + 0.3,
                     z: pos.z
                 });
 
-                ball.setAttribute("velocity", direction.multiplyScalar(-10));
+                ball.setAttribute("velocity", direction.multiplyScalar(-11));
+
+                ball.addEventListener("collide", this.impact);
 
                 scene.appendChild(ball);
             }
         })
+    },
+
+    impact: function (e) {
+        //ball element
+        var element = e.detail.target.el;
+
+        var elementHit = e.detail.body.el;
+
+        if (elementHit.id.includes("pin")) {
+            /*elementHit.setAttribute("material", {
+                opacity: 0,
+                transparent: true,
+            });
+
+            var impulse = new CANNON.Vec3(-1, 1, 0.5);
+            var worldPoint = new CANNON.Vec3().copy(element.getAttribute("position"));
+            elementHit.body.applyImpulse(impulse, worldPoint);
+
+            element.removeEventListener("collide", this.impact);
+            var scene = document.querySelector("#scene");
+            scene.removeChild(element);*/
+        }
     }
 })
